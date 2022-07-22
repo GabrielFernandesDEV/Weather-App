17f5335e478008a4508a9cb4c67fa2c83d173940
Gabriel Fernandes <gabrielfernandes.dev@gmail.com>
2022-07-22 17:02:25 -0300

    Updates

---
+++ precache-manifest.37f37b42cf30f8f0c56d1cd1ca1703ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4faa92e4d1e55fbff95",
+    "url": "/Weather-App/static/css/main.a44e0854.chunk.css"
+  },
+  {
+    "revision": "f4faa92e4d1e55fbff95",
+    "url": "/Weather-App/static/js/main.f4faa92e.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/Weather-App/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "5de2f0c9c3453caf6cc4",
+    "url": "/Weather-App/static/js/2.5de2f0c9.chunk.js"
+  },
+  {
+    "revision": "272e8af1c78272d0de2f",
+    "url": "/Weather-App/static/js/runtime~main.272e8af1.js"
+  },
+  {
+    "revision": "6b2b160ee4a582bdfea657eb38d35e2b",
+    "url": "/Weather-App/index.html"
+  }
+];
